44b294f0613a95dbc09fe5cce059aa10c75570a7
jotaeme890 <permanentjavi@gmail.com>
2022-10-06 10:18:38 +0200

    Ejercicio3

---
+++ Ejercicio3.java
@@ -0,0 +1,10 @@
+public class Ejercicio3 {
+    public static void main(String args[]){
+        String nombre = "Javier Miguel Martín";
+        String direccion = "Dirección: Calle Frederick Terman, 3, 29590 Málaga";
+        String numero = "000000000";
+        System.out.println("Mi nombre es: "+nombre);
+        System.out.println("Vivo en: "+direccion);
+        System.out.println("Mi número de teléfono es: "+numero);
+    }
+}
