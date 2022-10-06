3f602ae3130e51669159020023b6d0d032c5a7c8
jotaeme890 <permanentjavi@gmail.com>
2022-10-06 10:07:34 +0200

    Ejercicio1

---
+++ Ejercicio1.java
@@ -0,0 +1,13 @@
+public class Ejercicio1{
+    public static void main(String args[]){
+        int x = 144;
+        int y = 999;
+
+        System.out.println("x= "+x);
+        System.out.println("y= "+y);
+        System.out.println("La suma es: "+(x+y));
+        System.out.println("La resta es :"+(x-y));
+        System.out.println("La divisón es: "+(x/y));
+        System.out.println("La multiplicación es: "+(x*y));
+    }
+}
