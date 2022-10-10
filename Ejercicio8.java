f7b56c82ee1d5aa131c4c3654f7b28a41e675ade
jotaeme890 <permanentjavi@gmail.com>
2022-10-10 09:14:44 +0200

    Ejercicio8

---
+++ Ejercicio8.java
@@ -0,0 +1,15 @@
+public class Ejercicio8 {
+    public static void main (String args []){
+
+        char p = 'P';
+        char e = 'e';
+        char r = 'r';
+        char r2= 'r';
+        char o= 'o';
+
+        //If we put the caracter the variable do an addition of the asci´s valor of that caracter.
+        //The solution is put a "" at the biginning. 
+        String perro = ""+ p + e + r + r2 + o;
+        System.out.println(perro);
+    }
+}
