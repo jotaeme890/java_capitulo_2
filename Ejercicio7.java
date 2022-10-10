4b795ea931e4307f15edcc7554b938960216d152
jotaeme890 <permanentjavi@gmail.com>
2022-10-10 09:02:44 +0200

    Ejercicio7

---
+++ Ejercicio7.java
@@ -0,0 +1,13 @@
+public class Ejercicio7 {
+    public static void main (String args []){
+
+        char h = 'h';
+        char o = 'o';
+        char l = 'l';
+        char a = 'a';
+        String hola = "hola";
+
+        
+        System.out.print(h + "" + o + "" + l + "" + a + "" + " "+ hola);
+    }
+}
