0fa6379bac7e605255fda70aee509de4dc56361b
jotaeme890 <permanentjavi@gmail.com>
2022-10-10 08:53:27 +0200

    Ejercicio6

---
+++ Ejercicio6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main (String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Dime el total de la base de la factura: ");
+        double n = sc.nextInt();
+
+        
+        System.out.print("El total de la factura es: " + n+(n*0.21));
+
+        sc.close();
+    }
+}
