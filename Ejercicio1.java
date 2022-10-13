d7673c1f486fa26b756ef7c2dec42949d693b316
jotaeme890 <permanentjavi@gmail.com>
2022-10-13 08:48:31 +0200

    Ejercicio1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,3 +1,11 @@
+/**
+ *  1 Escribe un programa en el que se declaren las variables enteras x e y. Asígnales 
+ * los valores 144 y 999 respectivamente. A continuación, muestra por pantalla 
+ * el valor de cada variable, la suma, la resta, la división y la multiplicación.
+ * 
+ * @author jotaeme
+ */
+
 public class Ejercicio1{
     public static void main(String args[]){
         int x = 144;
