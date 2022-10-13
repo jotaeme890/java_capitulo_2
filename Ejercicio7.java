fd4d0822850fde5ac15916b72baeada4a11855aa
jotaeme890 <permanentjavi@gmail.com>
2022-10-13 08:49:14 +0200

    Ejercicio7

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -1,3 +1,11 @@
+/**
+ *   7 Escribe un programa que declare variables de tipo char y de tipo String. 
+ * Intenta mostrarlas por pantalla todas juntas en la misma línea y con una sola sentencia de Java 
+ * (con un solo println) ¿es posible?
+ * 
+ * @author jotaeme
+ */
+
 public class Ejercicio7 {
     public static void main (String args []){
 
