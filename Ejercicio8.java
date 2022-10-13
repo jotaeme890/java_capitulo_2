463ac2445ad9225a38e7e7e10aeab94270d7a777
jotaeme890 <permanentjavi@gmail.com>
2022-10-13 08:49:20 +0200

    Ejercicio8

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -1,3 +1,11 @@
+/**
+ *  8 Escribe un programa que declare 5 variables de tipo char. A continuación, 
+ * crea otra variable como cadena de caracteres y asígnale como valor la concatenación de las anteriores 5 variables. 
+ * Por último, muestra la cadena de caracteres por pantalla ¿Qué problemas te encuentras? ¿cómo lo has solucionado?
+ * 
+ * @author jotaeme
+ */
+
 public class Ejercicio8 {
     public static void main (String args []){
 
