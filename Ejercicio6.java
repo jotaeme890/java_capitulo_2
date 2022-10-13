65645ffc898b46faeedf6fa41e84fa385da0ba28
jotaeme890 <permanentjavi@gmail.com>
2022-10-13 08:49:05 +0200

    Ejercicio6

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,3 +1,10 @@
+/**
+ *  6 Escribe un programa que calcule el total de una factura a partir de la base imponible 
+ * (precio sin IVA). La base imponible estará almacenada en una variable.
+ * 
+ * @author jotaeme
+ */
+
 import java.util.Scanner;
 public class Ejercicio6 {
     public static void main (String args []){
